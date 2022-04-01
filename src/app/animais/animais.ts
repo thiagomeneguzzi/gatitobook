40ed4f4f9718b3709dfe3b48b487474b5f2be0ad
thiagomeneguzzi <thiago.meneguzzi@alunos.sc.senac.br>
2022-04-01 12:49:49 -0300

    interface animal

---
+++ src/app/animais/animais.ts
@@ -0,0 +1,10 @@
+export interface Animal {
+  id: number;
+  postDate: Date;
+  url: string;
+  description: string;
+  allowComments: boolean;
+  likes: number;
+  comments: number;
+  userId: number;
+}
